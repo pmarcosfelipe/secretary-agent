f19085ce55147dac26b9df93a5b30b940e734b2c
Marcos Felipe <p.marcosfelipe@gmail.com>
2026-08-19 06:58:11 +0100

    creating example function with paramenters

--- src/secretary.js
+++ src/secretary.js
@@ -2,9 +2,11 @@ import { GoogleGenAI } from "@google/genai";
 
 const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY || "" });
 
-const contents = [{ role: "user", parts: [{ text: "que dia é hoje?" }] }];
+const contents = [
+	{ role: "user", parts: [{ text: "What is the temperature in Brasil?" }] },
+];
 
-let response = await ai.models.generateContent({
+const response = await ai.models.generateContent({
 	model: "gemini-3.6-flash",
 	contents: contents,
 	config: {
@@ -13,8 +15,26 @@ let response = await ai.models.generateContent({
 				functionDeclarations: [
 					{
 						name: "getTodayDate",
-						description: "return the current date in the format YYYY-MM-DD",
-						// parameters: { type: "object", properties: {}, required: [] },
+						description: "Return the current date in the format YYYY-MM-DD",
+					},
+					{
+						name: "getCountryTemperature",
+						description: "return the temperature of a given country",
+						parameters: {
+							type: "OBJECT",
+							properties: {
+								country: {
+									type: "STRING",
+									description: "country name",
+								},
+								isCelsius: {
+									type: "BOOELAN",
+									description:
+										"Should return temperature in Celcius or not (Standard is true)",
+								},
+							},
+							required: ["country", "isCelsius"],
+						},
 					},
 				],
 			},
@@ -22,23 +42,24 @@ let response = await ai.models.generateContent({
 	},
 });
 
-contents.push(response.candidates[0].content);
+console.log(response.candidates[0].content.parts[0]);
+// contents.push(response.candidates[0].content.parts);
 
-contents.push({
-	role: "user",
-	parts: [
-		{
-			functionResponse: {
-				name: "getTodayDate",
-				response: { result: "2026-04-01" },
-			},
-		},
-	],
-});
+// contents.push({
+// 	role: "user",
+// 	parts: [
+// 		{
+// 			functionResponse: {
+// 				name: "getTodayDate",
+// 				response: { result: "2026-04-01" },
+// 			},
+// 		},
+// 	],
+// });
 
-response = await ai.models.generateContent({
-	model: "gemini-3.6-flash",
-	contents: contents,
-});
+// response = await ai.models.generateContent({
+// 	model: "gemini-3.6-flash",
+// 	contents: contents,
+// });
 
-console.log(response.candidates[0].content);
+// console.log(response.candidates[0].content);
